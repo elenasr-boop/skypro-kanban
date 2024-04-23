d8bbdefdc0d3a005a486b17c9710d461ef613551
Elena Rybakova <lelkya84@gmail.com>
2024-04-23 14:18:37 +0300

    finished components

---
+++ vite-project/src/components/main/main.jsx
@@ -0,0 +1,17 @@
+import Column from "../column/column";
+
+const Main = () => {
+    return ( <main className="main">
+    <div className="container">
+        
+        <div className="main__block">
+            <div className="main__content">
+                <Column></Column>
+            </div>
+        
+        </div>
+    </div>
+</main> );
+}
+ 
+export default Main;
