3a1db663848346bbb858b8fd9fdd7102ce7c1cb0
Elena Rybakova <lelkya84@gmail.com>
2024-06-04 11:59:52 +0300

    header

--- vite-project/src/components/header/header.jsx
+++ vite-project/src/components/header/header.jsx
@@ -1,31 +1,34 @@
 import { useState } from "react";
+import * as S from "./header.styled.js";
+import { Container } from "../main/main.styled.js";
+
 
 const Header = ( {onCardAdd}) => {
   const [isUserOpen, setIsUserOpen] = useState(false);
   const toggleDropdown = () => setIsUserOpen((prevState) => !prevState);
 
   return (
-    <header className="header">
-      <div className="container">
-        <div className="header__block">
-          <div className="header__logo _show _light">
+    <S.Header>
+      <Container>
+        <S.HeaderBlock>
+          <div className=" _show _light">
             <a href="" target="_self">
-              <img src="../../public/logo.png" alt="logo" />
+              <S.HeaderLogo src="../../public/logo.png" alt="logo" />
             </a>
           </div>
-          <div className="header__logo _dark">
+          <div className=" _dark">
             <a href="" target="_self">
-              <img src="../../public/logo_dark.png" alt="logo" />
+              <S.HeaderLogo src="../../public/logo_dark.png" alt="logo" />
             </a>
           </div>
-          <nav className="header__nav">
-            <button className="header__btn-main-new _hover01" id="btnMainNew" onClick={onCardAdd}>
-              <a href="#">Создать новую задачу</a>
-            </button>
-            <a href="#user-set-target" className="header__user _hover02" onClick={toggleDropdown}>
+          <S.HeaderNav>
+            <S.HeaderBtnMainNew className=" _hover01" id="btnMainNew" onClick={onCardAdd}>
+              <S.HeaderBtnMainNewA href="#">Создать новую задачу</S.HeaderBtnMainNewA>
+            </S.HeaderBtnMainNew>
+            <S.HeaderUser href="#user-set-target" className=" _hover02" onClick={toggleDropdown}>
               Ivan Ivanov
-            </a>
-            {isUserOpen && (<div
+            </S.HeaderUser>
+            {isUserOpen && (<S.HeaderPopUserSet
               className="header__pop-user-set pop-user-set"
               id="user-set-target"
             >
@@ -38,11 +41,11 @@ const Header = ( {onCardAdd}) => {
               <button type="button" className="_hover03">
                 <a href="#popExit">Выйти</a>
               </button>
-            </div>)}
-          </nav>
-        </div>
-      </div>
-    </header>
+            </S.HeaderPopUserSet>)}
+          </S.HeaderNav>
+        </S.HeaderBlock>
+      </Container>
+    </S.Header>
   );
 };
 
---
+++ vite-project/src/components/header/header.styled.js
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 100%;
+  margin: 0 auto;
+  background-color: #ffffff;
+`;
+
+export const HeaderBlock = styled.div`
+  height: 70px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  top: 0;
+  left: 0;
+  padding: 0 10px;
+`;
+
+export const HeaderNav = styled.nav`
+  max-width: 290px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HeaderLogo = styled.img`
+  width: 85px;
+`;
+
+export const HeaderBtnMainNew = styled.button`
+  width: 178px;
+  height: 30px;
+  border-radius: 4px;
+  background-color: #565eef;
+  color: #ffffff;
+  border: none;
+  font-size: 14px;
+  line-height: 1;
+  font-weight: 500;
+  margin-right: 20px;
+
+  @media (max-width: 495px) {
+    z-index: 3;
+    position: fixed;
+    left: 16px;
+    bottom: 30px;
+    top: auto;
+    width: calc(100vw - 32px);
+    height: 40px;
+    border-radius: 4px;
+    margin-right: 0;
+  }
+`;
+
+export const HeaderBtnMainNewA = styled.a`
+    color: #ffffff;
+`;
+
+export const HeaderUser = styled.a`
+  height: 20px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 20px;
+  color: #565eef;
+
+  &::after {
+    content: "";
+  display: block;
+  width: 6px;
+  height: 6px;
+  border-radius: 1px;
+  border-left: 1.9px solid #565eef;
+  border-bottom: 1.9px solid #565eef;
+  transform: rotate(-45deg);
+  margin: -6px 0 0 5px;
+  padding: 0;
+  }
+
+  &:hover {
+    color: #33399b;
+  }
+
+  &:hover::after {
+    border-left-color: #33399b;
+    border-bottom-color: #33399b;
+  }
+`;
+
+export const HeaderPopUserSet = styled.div`
+  display: none;
+  position: absolute;
+  top: 61px;
+  right: 0;
+  width: 213px;
+  height: 205px;
+  border-radius: 10px;
+  border: 0.7px solid rgba(148, 166, 190, 0.4);
+  background: #fff;
+  box-shadow: 0px 10px 39px 0px rgba(26, 56, 101, 0.21);
+  padding: 34px;
+  text-align: center;
+  z-index: 2;
+`
